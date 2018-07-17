5d4380c48e3292788174d83c062c28bf059ace85
Nicolas Widart <n.widart@gmail.com>
2018-07-17 20:06:22 +0200

    Creating a mocked video controller test

---
+++ techvideo/src/test/java/com/nwidart/techvideo/integration/controller/MockVideoControllerTest.java
@@ -0,0 +1,25 @@
+package com.nwidart.techvideo.integration.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class MockVideoControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testItReturnsAListOfVideos() throws Exception {
+    this.mockMvc.perform(get("/api/v1/videos"))
+        .andExpect(status().isOk());
+  }
+}
