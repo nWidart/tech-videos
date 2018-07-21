f7345abcc4bd72525cfdc3aa53b0d6f8089b67da
Nicolas Widart <n.widart@gmail.com>
2018-07-21 15:07:13 +0200

    Update the votes table

--- techvideo/src/main/resources/db/changelog/changelog-003-create-votes-table.sql
+++ techvideo/src/main/resources/db/changelog/changelog-003-create-votes-table.sql
@@ -7,7 +7,5 @@ CREATE TABLE votes
   id         int PRIMARY KEY AUTO_INCREMENT,
   email      varchar(255),
   video_id   int,
-  session_id int,
-  CONSTRAINT votes_fk_video_id FOREIGN KEY (video_id) REFERENCES videos (id),
-  CONSTRAINT votes_fk_session_id FOREIGN KEY (session_id) REFERENCES sessions (id)
+  CONSTRAINT votes_fk_video_id FOREIGN KEY (video_id) REFERENCES videos (id)
 );
