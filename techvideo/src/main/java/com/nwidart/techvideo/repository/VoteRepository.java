59aaf6ff5c3c6ed7e560c924906820fcd50b9812
Nicolas Widart <n.widart@gmail.com>
2018-07-21 16:22:24 +0200

    Adding method to get all votes for a specific session id

--- techvideo/src/main/java/com/nwidart/techvideo/repository/VoteRepository.java
+++ techvideo/src/main/java/com/nwidart/techvideo/repository/VoteRepository.java
@@ -1,8 +1,10 @@
 package com.nwidart.techvideo.repository;
 
 import com.nwidart.techvideo.entity.Vote;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
 
+  List<Vote> findAllBySessionId(Integer sessionId);
 }
---
+++ techvideo/src/test/java/com/nwidart/techvideo/integration/repository/VoteRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.nwidart.techvideo.integration.repository;
+
+import com.nwidart.techvideo.entity.Session;
+import com.nwidart.techvideo.entity.Video;
+import com.nwidart.techvideo.entity.Vote;
+import com.nwidart.techvideo.repository.VoteRepository;
+import java.time.OffsetDateTime;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class VoteRepositoryTest {
+
+  @Autowired
+  private VoteRepository repository;
+  @Autowired
+  private TestEntityManager testEntityManager;
+
+  @Test
+  public void findAllBySessionId() {
+    Session session1 = testEntityManager.persistAndFlush(new Session(OffsetDateTime.now()));
+    Session session2 = testEntityManager.persistAndFlush(new Session(OffsetDateTime.now()));
+    Video video = testEntityManager.persistAndFlush(new Video("My Title", "youtube.com/mytitle"));
+    testEntityManager.persistAndFlush(new Vote(video, session1.getId()));
+    testEntityManager.persistAndFlush(new Vote(video, session2.getId()));
+
+    List<Vote> votes = repository.findAllBySessionId(1);
+    Assert.assertEquals(1, votes.size());
+  }
+}
