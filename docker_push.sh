77e23857c4131722c452e4ccd5281975ddd26416
Nicolas Widart <n.widart@gmail.com>
2018-08-03 22:10:01 +0200

    Undo travis & docker

--- docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build
-cd techvideo
-./mvnw install dockerfile:build -DskipTests
-
-# Push
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push nwidart/techvideo
